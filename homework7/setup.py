87cf1398ce8d91a5a15b92ad6b484991b912bd74
sirko2a1 <134325906+sirko2a1@users.noreply.github.com>
2023-08-10 21:41:50 +0300

    commit2

--- homework7/setup.py
+++ homework7/setup.py
@@ -8,7 +8,7 @@ setup(
     install_requires=[], 
     entry_points={
         'console_scripts': [
-            'clean-folder = clean_folder.clean_folder.clean:main' #(модуль.підмодуль.функція:функція-запуску)
+            'clean-folder = clean_folder.clean:main' #(модуль.підмодуль.функція:функція-запуску)
         ]
     }
 )
